3ad652c82ce1f5522e992c792593bf9fdc8c1ece
MetalXeed <fullmetalxeed@gmail.com>
2024-10-19 20:15:22 +0200

    semana 4 - 04buclesMAR 1,5 -

--- src/Programacion01/bucles/04_buclesMAR01.kt
+++ src/Programacion01/bucles/04_buclesMAR01.kt
@@ -1,21 +1,29 @@
 package Programacion01.bucles
 
+import Programacion01.funciones.readlnToInt
+
 /*
  Aburrimiento en las sobremesas
 
-Los palillos y las cerillas son elementos típicos de gran cantidad de figuras, juegos y entretenimientos para la sobremesa. Todo el mundo, alguna vez, cuando la conversación desvaría e invade el tedio, hemos matado el tiempo haciendo formas geométricas, de mayor o menor tamaño dependiendo de cuanto material de construcción tengamos a mano.
+Los palillos y las cerillas son elementos típicos de gran cantidad de figuras, juegos y entretenimientos para la
+sobremesa. Todo el mundo, alguna vez, cuando la conversación desvaría e invade el tedio, hemos matado el tiempo
+            haciendo formas geométricas, de mayor o menor tamaño dependiendo de cuanto material de construcción
+            tengamos a mano.
 
 Por ejemplo, con 18 cerillas podemos formar la siguiente figura:
 
-Si la conversación es especialmente aburrida, se puede ir haciendo más y más grande, hasta que se acaben las cerillas, el espacio en la mesa… ¡o la conversación!
+Si la conversación es especialmente aburrida, se puede ir haciendo más y más grande, hasta que se acaben las
+cerillas, el espacio en la mesa… ¡o la conversación!
 
 Entrada
 
-La entrada estará compuesta de una sucesión de números positivos menores que 20.000. El último, que no deberá procesarse, será un 0.
+La entrada estará compuesta de una sucesión de números positivos menores que 20.000. El último, que no deberá
+procesarse, será un 0.
 
 Salida
 
-Por cada número de la entrada el programa deberá escribir cuántas cerillas son necesarias para formar un triángulo con ese número de cerillas en cada lado siguiendo el esquema de la figura.
+Por cada número de la entrada el programa deberá escribir cuántas cerillas son necesarias para formar un
+triángulo con ese número de cerillas en cada lado siguiendo el esquema de la figura.
 
 Por ejemplo:
 Entrada 	Resultado
@@ -26,3 +34,22 @@ Entrada 	Resultado
 0
 Respuesta:
  */
+fun piramide(n:Int,r:Int):Int {
+    var result:Int = r
+    result+=(n * 3)
+    if (n == 1) {
+        return (result)
+    }else {
+        return piramide(n - 1,result)
+    }
+}
+fun main() {
+    var altura = readln().toInt()
+    var resultado= 0
+    while(altura>0){
+
+        println(piramide(altura,resultado))
+
+        altura = readln().toInt()
+    }
+}
--- src/Programacion01/bucles/04_buclesMAR05.kt
+++ src/Programacion01/bucles/04_buclesMAR05.kt
@@ -1,15 +1,25 @@
 package Programacion01.bucles
+
+import kotlin.system.exitProcess
+
 /*
 CONTAR HASTA EL FINAL
 
-El día que, con cuatro años, Clara averiguó que los números son infinitos no le gustó nada. Le había costado mucho tiempo aprender a contar hasta 100, estaba empezando a entender cómo llegar hasta 1.000, y de repente se había enterado de que siempre tendría más y más números que aprender.
+El día que, con cuatro años, Clara averiguó que los números son infinitos no le gustó nada. Le había costado mucho
+tiempo aprender a contar hasta 100, estaba empezando a entender cómo llegar hasta 1.000, y de repente se había
+enterado de que siempre tendría más y más números que aprender.
 
-Le gustó tan poco ese descubrimiento que no pudo aceptarlo. A cualquier adulto que veía le preguntaba, sin previo aviso, por qué los números nunca se acaban. Aunque la respuesta era invariablemente la misma (un inquietante "porque siempre se puede sumar uno más a cualquier número"), ella no perdía la esperanza de que finalmente se rindieran y confesaran que todo había sido una broma de mal gusto.
+Le gustó tan poco ese descubrimiento que no pudo aceptarlo. A cualquier adulto que veía le preguntaba, sin previo
+aviso, por qué los números nunca se acaban. Aunque la respuesta era invariablemente la misma (un inquietante "porque
+siempre se puede sumar uno más a cualquier número"), ella no perdía la esperanza de que finalmente se rindieran y
+confesaran que todo había sido una broma de mal gusto.
 
 
 
 
-Un año después, en clase de religión, encontró la incrongruencia que estaba buscando. Esta tarde, en cuanto llegara a su casa pillaría a su padre en un renuncio y le obligaría a reconocer que, efectivamente, había un último número. La pregunta que le haría resonaba en su mente...
+Un año después, en clase de religión, encontró la incrongruencia que estaba buscando. Esta tarde, en cuanto llegara a
+su casa pillaría a su padre en un renuncio y le obligaría a reconocer que, efectivamente, había un último número. La
+pregunta que le haría resonaba en su mente...
 
 — Papá, ¿Dios puede contar hasta el final?
 
@@ -34,3 +44,37 @@ Entrada 	Resultado
 1.234.567   1.234.568
 Respuesta:
 */
+fun main(){
+    var carreo:Boolean = true
+    var entrada:String? = readLine()
+    while (entrada!=null) {
+        if(entrada.isNotEmpty()){
+            val cifras = mutableListOf<Int>()
+            for (c in entrada) {
+                if (c != '.') {
+                    cifras.add(c.toString().toInt())
+                }
+            }
+            carreo = true
+            var i = cifras.size - 1
+
+            while (carreo && i >= 0) {
+                cifras[i]+=1
+                if (cifras[i] == 10) {
+                    cifras[i] = 0
+                    i -= 1
+                } else
+                    carreo = false
+            }
+            //si terminamos el listado de cifras y nos llevamos una añadimos uno al principio 9 + 1 = 10
+            if (carreo) {
+                cifras.add(0, 1)
+            }
+            // Formatear el resultado con puntos para los millares
+            val resultado = cifras.joinToString("").reversed().chunked(3).joinToString(".").reversed()
+            println(resultado)
+        }
+        entrada = readLine()
+
+    }
+}
