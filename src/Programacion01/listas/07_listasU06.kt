11d0ea29e9f69c9b6b42a342bd07f26e94327dd1
MetalXeed <fullmetalxeed@gmail.com>
2024-11-15 16:21:42 +0100

    semana 7 - 07 ejercio 6 -

--- src/Programacion01/listas/07_listasU06.kt
+++ src/Programacion01/listas/07_listasU06.kt
@@ -3,16 +3,16 @@ package Programacion01.listas
 /*
 Siete picos
 
-En 1969 se inauguró el Parque de Atracciones de Madrid; su atracción estrella era la montaña rusa "Siete picos", que,
-tras 36 años de servicio y unos 77 millones de usuarios, fue desmontada en 2005 para, como ella mismo "dijo" en su carta
-de despedida, dejar paso a las nuevas generaciones.
+En 1969 se inauguró el Parque de Atracciones de Madrid; su atracción estrella era la montaña rusa "Siete picos",
+ que,tras 36 años de servicio y unos 77 millones de usuarios, fue desmontada en 2005 para, como ella mismo "dijo"
+en su carta de despedida, dejar paso a las nuevas generaciones.
 
-Curiosamente, pese a su nombre, aquella montaña rusa no tenía siete picos. Si llamamos "pico" a un punto del recorrido
-que está más alto que el inmediatamente anterior y el inmediatamente siguiente, entonces tenía como mucho 6 y ni
-siquiera las crónicas se ponen de acuerdo en esto.
+Curiosamente, pese a su nombre, aquella montaña rusa no tenía siete picos. Si llamamos "pico" a un punto del
+recorrido que está más alto que el inmediatamente anterior y el inmediatamente siguiente, entonces tenía como
+mucho 6 y ni siquiera las crónicas se ponen de acuerdo en esto.
 
-Dado el recorrido de varias montañas rusas, ¿puedes contar el número de picos? Ten en cuenta que las montañas rusas son
-circulares, y el punto de inicio de la entrada ¡podría ser un pico!
+Dado el recorrido de varias montañas rusas, ¿puedes contar el número de picos? Ten en cuenta que las montañas
+rusas son circulares, y el punto de inicio de la entrada ¡podría ser un pico!
 
 
 
@@ -21,8 +21,9 @@ Entrada
 
 El programa leerá de la entrada estándar múltiples casos de prueba, cada uno con la descripción de una montaña rusa.
 
-Una montaña rusa queda descrita por un primer número 2 ≤ n ≤ 1.000 indicando cuántas veces se ha anotado la altura del
-recorrido. A continuación vienen, en otra línea, n números positivos (menores que 1.000) con todas esas alturas.
+Una montaña rusa queda descrita por un primer número 2 ≤ n ≤ 1.000 indicando cuántas veces se ha anotado la altura
+ del recorrido. A continuación vienen, en otra línea, n números positivos (menores que 1.000) con todas esas
+alturas.
 
 La entrada termina con una montaña rusa sin alturas que no deberá procesarse.
 
@@ -43,3 +44,15 @@ Entrada 	Resultado
 0
 
  */
+fun calcularPicos():Int{
+    return 0
+}
+fun main(){
+    var entrada = readLine()
+    if(!entrada.isNullOrBlank()) {
+        while (!entrada[0].equals(0)) {
+            println(calcularPicos())
+            entrada = readLine()
+        }
+    }
+}
