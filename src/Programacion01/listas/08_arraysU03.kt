74b9b526e6088f6a8bf96a269e9c78801653c3a7
a24josegm <a24josegm@iessanclemente.net>
2024-11-18 22:31:14 +0100

    08 ArraysU - 03

--- src/Programacion01/listas/08_arraysU03.kt
+++ src/Programacion01/listas/08_arraysU03.kt
@@ -39,6 +39,90 @@ dD-3*A-E+   EDA
 EDA-3E*3A   EDA
 
 
+- INICIO
++ FINAL
+* DERECHA
+3 BORRA A LA DERECHA
 
 
+EDA
+EDA-333
+dD-3*A-E+
+EDA-3E*3A
+
+
+var arrTemp = CharArray(arrayEntrada.size-1)
+
+fun tecladoEstropeado(arrayEntrada:CharArray):CharArray{
+    var salida:CharArray= CharArray(arrayEntrada.size)
+    //var arrTemp:CharArray= CharArray(1){'0'}
+    var posicionCursor=0
+    var longitudSalida=0
+
+    for (i in arrayEntrada){
+        when(i){
+            '-' -> posicionCursor = 0
+            '+' -> posicionCursor = longitudSalida
+            '*' -> if (longitudSalida > posicionCursor)posicionCursor ++
+            '3' -> if (longitudSalida > posicionCursor){
+                for(j in posicionCursor until longitudSalida-1){
+                    salida[j] = arrayEntrada[j+1]
+                }
+            }
+            else -> {
+                for (j in (longitudSalida - 1) downTo posicionCursor) {
+                    salida[j + 1] = salida[j]
+                }
+                salida[posicionCursor] = i
+                longitudSalida++
+                posicionCursor++
+            }
+        }
+    }
+    return salida
+}
+fun main(){
+    var entrada = readLine()!!.toCharArray()
+    //println(tecladoEstropeado(entrada).joinToString())
+    while(entrada.isNotEmpty()){
+        println(tecladoEstropeado(entrada).joinToString(""))
+        entrada = readLine()!!.toCharArray()
+    }
+}
  */
+fun tecladoEstropeado(arrayEntrada: CharArray): CharArray {
+    var salida = CharArray(arrayEntrada.size)
+    var posicionCursor = 0
+    var longitudSalida = 0
+    for (i in arrayEntrada) {
+        when (i) {
+            '-' -> posicionCursor = 0
+            '+' -> posicionCursor = longitudSalida
+            '*' -> if (posicionCursor < longitudSalida) posicionCursor++
+            '3' -> if (posicionCursor < longitudSalida) {
+                for (j in posicionCursor until longitudSalida - 1) {
+                    salida[j] = salida[j + 1]
+                }
+                longitudSalida --
+            }
+            else -> {
+                for (j in (longitudSalida - 1) downTo posicionCursor) {
+                    salida[j + 1] = salida[j]
+                }
+                salida [posicionCursor] = i
+                longitudSalida ++
+                posicionCursor++
+            }
+        }
+    }
+    return salida.copyOfRange(0, longitudSalida)
+}
+
+fun main() {
+    var entrada = readLine()
+    while (!entrada.isNullOrEmpty()) {
+        var arrayEntrada =entrada.toCharArray()
+        println(tecladoEstropeado(arrayEntrada).joinToString(""))
+        entrada = readLine ()
+    }
+}
