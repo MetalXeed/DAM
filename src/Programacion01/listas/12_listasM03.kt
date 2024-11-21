3c48350c94bf9ff3029ab49e74717a224462e742
a24josegm <a24josegm@iessanclemente.net>
2024-11-21 23:50:09 +0100

    12 - 03

--- src/Programacion01/listas/12_listasM03.kt
+++ src/Programacion01/listas/12_listasM03.kt
@@ -7,15 +7,21 @@ Generar el tablero de buscaminas
 
 
 
-Cuando jugamos al buscaminas, el progreso del juego se va dirigiendo en base a un tablero precalculado que contiene las minas y la cantidad de celdas vecinas con minas que tiene cada celda.
+Cuando jugamos al buscaminas, el progreso del juego se va dirigiendo en base a un tablero precalculado que
+contiene las minas y la cantidad de celdas vecinas con minas que tiene cada celda.
 Entrada:
 
 Una serie de tableros.
 
-Cada tablero se describe con dos líneas. En la primera se indica las dimensiones de filas F con F>0 y de columnas C con C>0 separadas por un espacio. En la segunda fila, separadas por un espacio en blanco,  se indican las coordenadas (f,c) de las minas del tablero con valores 0<= f < F y 0 <= c < C. Se garantiza que los valores de la entrada son congruentes, por ejemplo, las dimensiones no son negativas ni las minas que se colocan fuera del tablero.
-Salida:
+Cada tablero se describe con dos líneas. En la primera se indica las dimensiones de filas F con F>0 y de columnas
+C con C>0 separadas por un espacio. En la segunda fila, separadas por un espacio en blanco,  se indican las
+coordenadas (f,c) de las minas del tablero con valores 0<= f < F y 0 <= c < C. Se garantiza que los valores de la
+entrada son congruentes, por ejemplo, las dimensiones no son negativas ni las minas que se colocan fuera del
+tablero.
 
-Tablero una vez colocadas las minas de la entrada. Las minas se representan con el dígito 9. Las celdas sin minas alrededor con 0.
+Salida:
+Tablero una vez colocadas las minas de la entrada. Las minas se representan con el dígito 9. Las celdas sin minas
+alrededor con 0.
 
 Por ejemplo:
 Entrada
@@ -58,3 +64,50 @@ Resultado
 ------------------------------------
 
  */
+fun crearTablero(valoresTablero:List<Int>):List<List<Int>>{
+    var filas    = valoresTablero[0]
+    var columnas = valoresTablero[1]
+
+    return List(filas){MutableList(columnas) {0} }
+}
+fun colocarMinas(tablero:List<List<Int>>,minas:List<Int>):List<List<Int>> {
+    val filas = tablero.size
+    val columnas = tablero[0].size
+    val resultado = tablero.map { it.toMutableList() }
+
+    for (i in minas.indices step 2) {
+        val f = minas[i]
+        val c = minas[i + 1]
+
+        resultado[f][c] = 9 //añadimos las minas
+
+        for (i in -1..1) { //sumamos 1 a las posiciones del tablero adjacentes que no tengan mina.
+            for (j in -1..1) {
+                val nFila    = f + i
+                val nColumna = c + j
+                if (nFila in 0 until filas && nColumna in 0 until columnas && resultado[nFila][nColumna] != 9)
+                    resultado[nFila][nColumna]++
+            }
+        }
+    }
+    return resultado
+}
+
+fun imprimirMinas(partida:List<List<Int>>){
+    for (fila in partida){
+        println(fila.joinToString(" "))
+    }
+}
+fun main(){
+    var input = readLine()
+
+    while(!input.isNullOrBlank()){
+        var valoresTablero = input.split(" ").map { it.toInt() }
+        var minas          = readLine()!!.split(" ").map { it.toInt() }
+        var partida        = colocarMinas(crearTablero(valoresTablero), minas)
+
+        imprimirMinas(partida)
+        println("------------------------------------")
+        input = readLine()
+    }
+}
