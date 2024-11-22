a02db037d8a32e8566a8af2601795512419c2ba1
a24josegm <a24josegm@iessanclemente.net>
2024-11-22 01:03:58 +0100

    12 - 04 planteamiento

--- src/Programacion01/listas/12_listasM04.kt
+++ src/Programacion01/listas/12_listasM04.kt
@@ -5,13 +5,19 @@ Doblando un papel
 
 (En kotlin)
 
-Hay distintos tipos de simetrías,   la simetría respecto a una recta o simetría axial quizá sea la más conocida y es la que vamos a usar en este problema.
+Hay distintos tipos de simetrías,   la simetría respecto a una recta o simetría axial quizá sea la más conocida
+y es la que vamos a usar en este problema.
 
-Decimos que una figura es simétrica respecto a una recta cuando cada punto a un lado de esa recta tiene otro punto al otro lado y a la misma distancia de esa recta.
+Decimos que una figura es simétrica respecto a una recta cuando cada punto a un lado de esa recta tiene otro punto
+al otro lado y a la misma distancia de esa recta.
 
-Si queremos saber si una imagen presenta simetría respecto a una recta y la tenemos en una hoja de papel solo tenemos que doblarla por la recta. Si al doblar la hoja las figuras coinciden es que son simétricas respecto a esa recta. Si no coinciden, no lo son. La recta para establecer la simetría puede ser vertical, horizontal o diagonal con cualquier inclinación. En este problema usaremos una recta vertical.
+Si queremos saber si una imagen presenta simetría respecto a una recta y la tenemos en una hoja de papel solo
+tenemos que doblarla por la recta. Si al doblar la hoja las figuras coinciden es que son simétricas respecto a
+esa recta. Si no coinciden, no lo son. La recta para establecer la simetría puede ser vertical, horizontal o
+diagonal con cualquier inclinación. En este problema usaremos una recta vertical.
 
-Pero nosotros lo haremos con matrices de caracteres .... Escribe las funciones que necesitan los test siguientes..
+Pero nosotros lo haremos con matrices de caracteres .... Escribe las funciones que necesitan los test
+siguientes..
 
 Por ejemplo:
 Prueba
@@ -55,3 +61,36 @@ Resultado
 * *     * *
 *         *
  */
+
+fun imprimirFiguras(){
+    //como tiene el mismo tamaño x puedo hacer un bucle con 2 anidados facilmente creo.
+}
+fun getSimetrica(){
+    //convertir alista mutable.
+    //la matriz tiene el mismo tamaño
+    //los valores dentro de las listas se pueden invertir utilizando downto.
+}
+
+fun main() {
+    var original = arrayOf(
+        charArrayOf('*', ' ', ' '),
+        charArrayOf('*', '*', ' '),
+        charArrayOf('*', '*', '*')
+    )
+    imprimirFiguras(original, getSimetrica(original))
+
+    original = arrayOf(
+        charArrayOf(' ', '*',' ',' '),
+        charArrayOf('*', '*','*','*'),
+        charArrayOf(' ', '*',' ',' ')
+    )
+    imprimirFiguras(original, getSimetrica(original))
+    original = arrayOf(
+        charArrayOf('*', ' ', ' '),
+        charArrayOf('*', '*', ' '),
+        charArrayOf('*', ' ', '*'),
+        charArrayOf('*', '*', ' '),
+        charArrayOf('*', ' ', ' '),
+    )
+    imprimirFiguras(original, getSimetrica(original))
+}
