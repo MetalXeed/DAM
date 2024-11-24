9faf4e3f48846d35f8752b404e5cfc9c35079f21
a24josegm <a24josegm@iessanclemente.net>
2024-11-24 20:21:49 +0100

    11 - 04

--- src/Programacion01/listas/11_listasMA04.kt
+++ src/Programacion01/listas/11_listasMA04.kt
@@ -38,6 +38,52 @@ Entrada 	                    Resultado
 
 
 
+7 7 8
+7 2 3 3 4 3 4 4 3 5 4 5 1 7 2 7
+0 0 0
 
 
- */
+*/
+fun calcularSombras(matriz:MutableList<CharArray>):Int{
+    //val mapa = mutableMapOf<Int,Int>() //lo uso para guardar cuantas posiciones hay rodeadas con cuantas minas
+    val filas  = matriz.size
+    val columnas = matriz[0].size
+    var sombrasTotales = 0
+
+    for (l in 0 until filas) {
+        for (m in 0 until columnas) {
+            if (matriz[l][m]=='A'){
+                for (n in -1..+1){
+                    for(o in -1 .. +1){
+                        if((l+n) in 0 until filas && (m+o)in 0 until columnas && (matriz[l+n][m+o]!='S') && (matriz[l+n][m+o]!='A')){
+                            matriz[l+n][m+o]='S'
+                            sombrasTotales++
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //println(matriz)
+    return sombrasTotales
+}
+fun main(){
+
+    var entrada = readLine()
+    while(!entrada.isNullOrBlank() && !entrada.equals("0 0 0") ){
+        val lista = entrada.split(" ")
+        var columnas = lista[0].toInt()
+        var filas = lista[1].toInt()
+        var nArboles = lista[2].toInt()
+        var matriz = MutableList(filas){CharArray(columnas){'-'}} // '-' no sombra 'S' 'A' arbol
+
+        var listaArboles = readLine()!!.split(" ").map { it.toInt() }
+        for (a in 0 until nArboles){
+            var columna = listaArboles[2*a]-1
+            var fila = listaArboles[2*a+1]-1
+            matriz [fila][columna] = 'A'
+        }
+        println(calcularSombras(matriz))
+        entrada = readLine()
+    }
+}
