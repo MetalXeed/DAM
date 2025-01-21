8a60394409f175aabeebc1abeeac8eeab611fcd5
a24josegm <a24josegm@iessanclemente.net>
2025-01-22 00:35:49 +0100

    segunda eval 02.

--- src/Programacion01/POO/sintaxisBasica/14_sintaxisBasica11.kt
+++ src/Programacion01/POO/sintaxisBasica/14_sintaxisBasica11.kt
@@ -23,21 +23,38 @@ Prueba 	Resultado
 [(BASTOS, 1), (BASTOS, 2), (BASTOS, 3), (BASTOS, 4), (BASTOS, 5), (BASTOS, 6), (BASTOS, 7), (BASTOS, 10), (BASTOS, 11), (BASTOS, 12), (COPAS, 1), (COPAS, 2), (COPAS, 3), (COPAS, 4), (COPAS, 5), (COPAS, 6), (COPAS, 7), (COPAS, 10), (COPAS, 11), (COPAS, 12), (ESPADAS, 1), (ESPADAS, 2), (ESPADAS, 3), (ESPADAS, 4), (ESPADAS, 5), (ESPADAS, 6), (ESPADAS, 7), (ESPADAS, 10), (ESPADAS, 11), (ESPADAS, 12), (OROS, 1), (OROS, 2), (OROS, 3), (OROS, 4), (OROS, 5), (OROS, 6), (OROS, 7), (OROS, 10), (OROS, 11), (OROS, 12)]
 0
  */
-enum class Palos(){
+enum class Palos{
     BASTOS, COPAS, ESPADAS, OROS
 }
-enum class numeros(){
-    //'1','2','3','4','5','6','7','8','9','10','11','12'
-}
-class Carta(){
-
+class Carta(var palo:Palos, var numero:Int){
+    override fun toString(): String {
+        return "($palo, $numero)"
+    }
 }
 class Baraja(){
+    val p = Palos.values()
+    var cartas:MutableList<Carta> = mutableListOf()//lista de cartas
+    init{
 
+        for (f in p){
+            for (i in 1..7){
+                cartas.add(Carta(f,i))
+
+            }
+            for (j in 10..12){
+                cartas.add(Carta(f,j))
+
+            }
+        }
+    }
+
+    override fun toString(): String {
+        return cartas.toString()
+    }
 }
 fun main(){
     val baraja = Baraja()
-    //println(baraja.cartas)//usa toString de lista
-    //val c:Carta=baraja.cartas[0]
-    //println(c.palo.ordinal)
+    println(baraja.cartas)//usa toString de lista
+    val c:Carta=baraja.cartas[0]
+    println(c.palo.ordinal)
 }
---
+++ src/Programacion01/POO/sintaxisBasica/14_sintaxisBasica12.kt
@@ -0,0 +1,46 @@
+package Programacion01.POO.sintaxisBasica
+
+/*
+data class
+
+Las clases User y Persona tienen los campos nombre y edad.
+Escribe estas clases para que funcionen los tests.
+
+Por ejemplo:
+Prueba 	Resultado
+
+fun main() {
+    val p1= Persona("Yo", 25)
+    val p2= Persona("Yo", 25)
+    println(p1==p2)
+    val user1 = User("A", 25)
+    val user2 = User("B", 30)
+    val user3 = User("A", 25)
+
+
+    println(user1 == user2)
+    println(user1 == user3)
+
+    val user4 = user1.copy()
+    println(user4)
+
+    val user5 = user1.copy(edad = 35)
+    println(user5)
+}
+
+
+
+false
+false
+true
+User(nombre=A, edad=25)
+User(nombre=A, edad=35)
+
+ */
+
+class Persona(var nombre:String, var edad:Int){
+
+}
+class User(var nombre:String, var edad:Int){
+
+}
