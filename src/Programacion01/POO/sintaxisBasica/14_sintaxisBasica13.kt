745abcfe394f6aa8ffee20e1a930dbc02c436b6f
a24josegm <a24josegm@iessanclemente.net>
2025-01-23 00:29:24 +0100

    segunda eval 03.

---
+++ src/Programacion01/POO/sintaxisBasica/14_sintaxisBasica13.kt
@@ -0,0 +1,39 @@
+package Programacion01.POO.sintaxisBasica
+
+/*
+Desestructuracion I
+
+para practicar desestructuración
+
+Las clases User y Persona tienen los campos nombre y edad.
+Escribe estas clases para que funcionen los tests.
+
+Por ejemplo:
+Prueba 	Resultado
+
+fun main() {
+    val (nombreP,edadP)= Persona("Yo", 25)
+
+    val (nombreU,edadU) = User("A", 25)
+    println("$nombreP $edadP    $nombreU $edadU")
+}
+
+
+
+Yo 25    A 25
+
+ */
+
+data class Persona(var nombre:String, var edad:Int)
+
+data class User(var nombre:String, var edad:Int){
+    override fun toString(): String {
+        return "User(nombre=$nombre, edad=$edad)"
+    }
+}
+fun main() {
+    val (nombreP,edadP)= Persona("Yo", 25)
+
+    val (nombreU,edadU) = User("A", 25)
+    println("$nombreP $edadP    $nombreU $edadU")
+}
---
+++ src/Programacion01/POO/sintaxisBasica/14_sintaxisBasica14.kt
@@ -0,0 +1,29 @@
+package Programacion01.POO.sintaxisBasica
+
+/*
+Desestructuracion II
+
+Entran una serie de Strings separados por un espacio
+
+Se imprimen los tres más pequeños
+
+Resuelve usando desestructuración
+
+Por ejemplo:
+Entrada 	Resultado
+
+alberto roberto norberto filiberto
+
+
+
+alberto filiberto norberto
+
+ */
+fun main(){
+    val entrada = readLine()
+    if(!entrada.isNullOrBlank()){
+        var listado = entrada!!.split(" ")
+        val (primera, segunda, tercera) = listado.sorted().take(3)
+        println("$primera $segunda $tercera")
+    }
+}
